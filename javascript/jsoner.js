f0afd4abc5e2d6ddf1893542b51c1189e636c1a7
nik@mihalick <nik@mihalick.name>
2011-09-29 23:21:41 -0400

    added a descriptive comment.

--- javascript/jsoner.js
+++ javascript/jsoner.js
@@ -1,3 +1,8 @@
+/**
+ * jsoner is a wrapper for the JSON stringify and parse methods. if the passed
+ * value cannot be converted, it will be returned as-is. 
+ *
+ */
 var jsoner = function() {
     var that = {};
     that.convert = function(o) {

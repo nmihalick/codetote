c9f7d348137bad85d54f1c23484316b892cc765e
nik@mihalick <nik@mihalick.name>
2011-09-29 23:25:26 -0400

    added a descriptive comment.

--- javascript/tester.js
+++ javascript/tester.js
@@ -1,3 +1,13 @@
+/**
+ * simple unit testing framework for javascript. requires a web browser. the
+ * best way to use this is to embed it in an empty html file.
+ *
+ * not very robust yet. it was simpler to write this than to setup, learn, and
+ * use a framework built by someone else. 
+ *
+ * there are no test classes or functions. just assert_equals. what else do you
+ * really need?
+ */
 var tester = function() {
     var that = {};
     that.assert_equals = function(t1, t2, n) {
